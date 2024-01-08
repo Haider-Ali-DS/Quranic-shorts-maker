eeef14d43e29100829c56aad2d06f8b9850aee69
Haider-Ali-Ds <haiderali.tech_786@outlook.com>
2024-01-08 06:02:28 +0500

    fixing subtitle timing issues

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,15 +1,24 @@
 use std::{
+    error::Error,
     fs::{self, File},
-    io::BufReader,
+    io::{self, BufReader, Write},
     process::Command,
+    time::Duration,
 };
 
 use xml::{attribute::OwnedAttribute, reader::XmlEvent, EventReader};
 
-use crate::audio_helper::*;
+use crate::{AUDIO_DIR, TEXT_DIR};
+
+#[derive(Debug)]
+pub struct SubtitleData {
+    pub start_time: String,
+    pub end_time: String,
+    pub text: String,
+}
 
 pub fn read_arabic_quran(sura_index_target: &str, aya_index_target: &str) -> String {
-    let file = File::open("resources/text/urdu.xml").unwrap();
+    let file = File::open(format!("{}/urdu.xml", TEXT_DIR)).unwrap();
     let file = BufReader::new(file);
 
     let parser = EventReader::new(file);
@@ -51,15 +60,141 @@ fn get_attribute_value(attributes: &[OwnedAttribute], name: &str) -> Option<Stri
         .map(|attr| attr.value.clone())
 }
 
-pub fn make_short(bg_image: &str, text: &str, audio_path: &str) {
-    let output_path = "generated-videos/output.mp4";
-    let title = "drawtext=text=Al-Quran:fontcolor=white:fontsize=60:x=(w-text_w)/2:y=80";
-    let sub_title = "drawtext=text=Translation:fontcolor=white:fontsize=40:x=(w-text_w)/2:y=135";
-    let seperator =
-        "drawtext=text=❀----------❤----------❀:fontcolor=white:fontsize=30:x=(w-text_w)/2:y=175";
-    let _ = subtitle_generator(text, audio_path);
-    let subtitle_filter = "subtitles=filename=output.ass:fontsdir=resources/fonts/";
+pub fn get_full_audio_and_text(
+    surah: &str,
+    start_aya: &str,
+    end_aya: &str,
+    temp_file_path: &str,
+) -> Vec<SubtitleData> {
+    let start = start_aya.parse::<u16>().unwrap();
+    let end = end_aya.parse::<u16>().unwrap();
+    let mut file = File::create(temp_file_path).unwrap();
+    let mut subtitle_timestamps: Vec<SubtitleData> = vec![];
+    let mut current_start_time = Duration::new(0, 0);
+
+    for i in start..=end {
+        let audio_path = format!("{}/arabic/{:0>3}{:0>3}.mp3", AUDIO_DIR, surah, i);
+        writeln!(file, "file '{}'", audio_path).unwrap();
+
+        let text = read_arabic_quran(surah, &format!("{}", i));
+        let duration_f64 = get_audio_duration(&audio_path).unwrap();
+        let duration = parse_duration(duration_f64);
+        let end_time = duration;
+
+        subtitle_timestamps.push(SubtitleData {
+            start_time: format_duration(current_start_time),
+            end_time: format_duration(end_time),
+            text,
+        });
+
+        current_start_time = current_start_time + end_time;
+    }
+    subtitle_timestamps
+}
+
+fn parse_duration(duration_secs: f64) -> Duration {
+    let whole_seconds = duration_secs.trunc() as u64;
+    let nanoseconds = ((duration_secs.fract() * 1_000_000_000.0) as u32) % 1_000_000_000;
+
+    Duration::new(whole_seconds, nanoseconds)
+}
+
+fn format_duration(duration: Duration) -> String {
+    let hours = duration.as_secs() / 3600;
+    let minutes = (duration.as_secs() % 3600) / 60;
+    let seconds = duration.as_secs() % 60;
+    let milliseconds = duration.subsec_millis();
+
+    format!(
+        "{:02}:{:02}:{:02}.{}",
+        hours,
+        minutes,
+        seconds,
+        milliseconds
+            .to_string()
+            .get(0..2)
+            .unwrap_or("00")
+            .to_string()
+    )
+}
+
+fn get_audio_duration(file_path: &str) -> Result<f64, Box<dyn std::error::Error>> {
+    let output = Command::new("ffprobe")
+        .args([
+            "-v",
+            "error",
+            "-show_entries",
+            "format=duration",
+            "-of",
+            "default=noprint_wrappers=1:nokey=1",
+            file_path,
+        ])
+        .output()?;
+
+    let binding = String::from_utf8(output.stdout)?;
+    let duration_str = binding.trim();
+    let duration = duration_str.parse::<f64>()?;
+    Ok(duration)
+}
+
+pub fn concatenate_mp3(list_file_path: &str, output_path: &str) -> io::Result<()> {
     Command::new("ffmpeg")
+        .args([
+            "-f",
+            "concat",
+            "-safe",
+            "0",
+            "-i",
+            list_file_path,
+            "-c",
+            "copy",
+            output_path,
+        ])
+        .output()
+        .expect("Failed to execute FFmpeg command");
+
+    Ok(())
+}
+
+pub fn make_subtitle_file(subtitles: Vec<SubtitleData>, output: &str) {
+    let mut dialogues = String::new();
+    for subtitle in subtitles {
+        let dialogue = format! {
+            "Dialogue: 0,{},{},Default,,0,0,0,,{}\n",
+            subtitle.start_time, subtitle.end_time, subtitle.text
+        };
+        dialogues.push_str(&dialogue);
+    }
+    let ass_content = format!(
+        "[Script Info]\n\
+        ScriptType: v4.00+\n\
+        \n\
+        [V4+ Styles]\n\
+        Format: Name, Fontname, Fontsize, PrimaryColour, SecondaryColour, OutlineColour, BackColour, Bold, Italic, Underline, StrikeOut, ScaleX, ScaleY, Spacing, Angle, BorderStyle, Outline, Shadow, Alignment, MarginL, MarginR, MarginV, Encoding\n\
+        Style: Default, Noto Naskh Arabic, 18, &H00FFFFFF, &H000000FF, &H00000000, &H00000000, 0, 0, 0, 0, 100, 100, 0, 0, 1, 1, 0, 2, 10, 10, 10, 1\n\
+        \n\
+        [Events]\n\
+        Format: Layer, Start, End, Style, Name, MarginL, MarginR, MarginV, Effect, Text\n\
+        {}",
+        dialogues
+    );
+
+    // Write to the ASS file
+    let mut file = File::create(output).unwrap();
+    file.write_all(ass_content.as_bytes()).unwrap();
+}
+
+pub fn make_short(bg_image: &str, subtitle_path: &str, audio_path: &str) {
+    let output_path = "generated-videos/output.mp4";
+    let title = "drawtext=text=Al-Quran:fontfile=resources/fonts/arabic.ttf:fontcolor=white:fontsize=60:x=(w-text_w)/2:y=80";
+    let sub_title = "drawtext=text=Translation:fontfile=resources/fonts/arabic.ttf:fontcolor=white:fontsize=40:x=(w-text_w)/2:y=135";
+    let seperator =
+        "drawtext=text=❀----------❤----------❀:fontfile=resources/fonts/arabic.ttf:fontcolor=white:fontsize=30:x=(w-text_w)/2:y=175";
+    let subtitle_filter = format!(
+        "subtitles=filename={}:fontsdir=resources/fonts/",
+        subtitle_path
+    );
+    let output = Command::new("ffmpeg")
         .args([
             "-loop",
             "1",
@@ -80,5 +215,6 @@ pub fn make_short(bg_image: &str, text: &str, audio_path: &str) {
         ])
         .output()
         .expect("Failed to execute FFmpeg command");
+    println!("output {:?}", output);
     let _ = fs::remove_file("output.ass");
 }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,42 @@
-use std::error::Error;
+use std::{error::Error, fs};
 
 use helpers::*;
 
-pub mod audio_helper;
 mod helpers;
+
+pub const AUDIO_DIR: &str = "resources/audios";
+pub const BG_DIR: &str = "resources/backgrounds";
+pub const TEXT_DIR: &str = "resources/texts";
+pub const FONT_DIR: &str = "resources/fonts";
+
+pub enum AudioType {
+    Arabic,
+    English,
+    Urdu,
+}
+pub enum TextType {
+    Arabic,
+    English,
+    Urdu,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let surah = "2";
-    let aya = "13";
-    let audio_path = format!("resources/audios/arabic/{:0>3}{:0>3}.mp3", surah, aya);
-    let input_text = read_arabic_quran(surah, aya);
-    let input_image = "resources/backgrounds/desert.jpg";
-    make_short(input_image, &input_text, &audio_path);
+    let surah = "1";
+    let start_aya = "1";
+    let end_aya = "7";
+    let temp_file_path = "mp3files.txt";
+    let concatenated_audio = "audio.mp3";
+    let subtitle_path = "subtitle.ass";
+    let input_image = format!("{}/desert.jpg", BG_DIR);
+
+    let subtitles = get_full_audio_and_text(surah, start_aya, end_aya, temp_file_path);
+    concatenate_mp3(temp_file_path, concatenated_audio).unwrap();
+    make_subtitle_file(subtitles, subtitle_path);
+    make_short(&input_image, subtitle_path, concatenated_audio);
+
+    fs::remove_file(temp_file_path).unwrap();
+    fs::remove_file(concatenated_audio).unwrap();
+    fs::remove_file(subtitle_path).unwrap();
     Ok(())
 }
